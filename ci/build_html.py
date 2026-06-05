9678b396ab22cb556f60ba85ed462f33562a820c
Eonos Lovos <enlvenlvenlv@gmail.com>
2026-06-05 18:31:23 +0000

    indentation

--- ci/build_html.py
+++ ci/build_html.py
@@ -1101,7 +1101,7 @@ function playAudio(btn) {
                 </tr>
             '''
         html += "</tbody></table></div>"
-   html += f'<div class="nav-links">\
+    html += f'<div class="nav-links">\
   <a class="left_link" href="{link_prev((data[0].get("book", "Bible"), chapter, -1))}">&lt;&lt;&lt;&lt;&lt;</a>\n  <a class="center_link" href="#bible-search">↑↑↑</a>\n  <a class="right_link" href="{link_next((data[0].get("book", "Bible"), chapter, -1))}">&gt;&gt;&gt;&gt;&gt;</a>\n  </div>'
     html += "</body></html>"
     return html
